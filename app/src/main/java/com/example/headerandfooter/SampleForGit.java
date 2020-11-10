c91475ded10df11cb69c948b53f6594ba77a15d5
Poonam Bora <poonam.bora@abmra.in>
2020-11-10 12:54:57 +0530

    New Activity Added

---
+++ app/src/main/java/com/example/headerandfooter/SampleForGit.java
@@ -0,0 +1,14 @@
+package com.example.headerandfooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SampleForGit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_for_git);
+    }
+}
